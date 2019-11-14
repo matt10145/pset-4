06e034d7e64e273f280cfa7d7dd9560cccd49529
Matthew Hui <matt10145h@gmail.com>
2019-11-14 10:44:27 -0500

    finishes exercise 1

--- src/sum.js
+++ src/sum.js
@@ -1,15 +1,13 @@
 const readlineSync = require("readline-sync");
 
-let LOWER_BOUND = null;
-let UPPER_BOUND = null;
+let LOWER_BOUND = 0;
+let UPPER_BOUND = 0;
 
+console.log("");
 do {
-  LOWER_BOUND = readlineSync.question("\nLower bound: ");
+  LOWER_BOUND = readlineSync.question("Lower bound: ");
   UPPER_BOUND = readlineSync.question("Upper bound: ");
-  if ((LOWER_BOUND > Number.MAX_SAFE_INTEGER || LOWER_BOUND < Number.MIN_SAFE_INTEGER) || (UPPER_BOUND > Number.MAX_SAFE_INTEGER || UPPER_BOUND < Number.MIN_SAFE_INTEGER)) {
-    console.log("Invalid.");
-  }
-} while (LOWER_BOUND > UPPER_BOUND);
+} while (LOWER_BOUND > UPPER_BOUND || LOWER_BOUND > Number.MAX_SAFE_INTEGER || LOWER_BOUND < Number.MIN_SAFE_INTEGER || UPPER_BOUND > Number.MAX_SAFE_INTEGER || UPPER_BOUND < Number.MIN_SAFE_INTEGER);
 
 let sum = 0;
 for (let i = LOWER_BOUND; i <= UPPER_BOUND; i++) {
@@ -21,8 +19,4 @@ for (let i = LOWER_BOUND; i <= UPPER_BOUND; i++) {
   }
 }
 
-console.log(sum.toLocaleString('en-US', {minimumFractionDigits :2, maximumFractionDigits: 2}));
-
-
-
-//NOT DONE
+console.log(`\n${sum.toLocaleString('en-US')}.`);
