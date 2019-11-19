294a4ad1d73c76fb075deb2d55fe2a71dc6b9078
matth101 <mhui@ucvts.org>
2019-11-18 22:07:27 -0500

    finishes exercise 6

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,13 +1,25 @@
 const readlineSync = require("readline-sync");
 
+const UPPER_THRESHOLD = 78;
 let previousNumber = 1;
 let previousNumber2 = 0;
+let input = 0;
 let sum = 0;
 
+console.log("");
 do {
-    let input = readlineSync.question("Positive integer: ");
-} while (input < 1 || input > 78 || Number.isNaN(input) || input % 1 != 0)
+    input = Number(readlineSync.question("Positive integer: "));
+} while (input < 1 || input > UPPER_THRESHOLD || Number.isNaN(input) || input % 1 != 0)
 
-for (let i = 1; i <= input; i++) {
-    if (prime)
+if (input == 2) {
+    sum = 1;
 }
+else if (input > 2) {
+    for (let i = 1; i <= input - 2; i++) {
+        sum = previousNumber + previousNumber2;
+        previousNumber2 = previousNumber;
+        previousNumber = sum;
+    }
+}
+
+console.log(`\n${sum.toLocaleString('en-US')}.`);
